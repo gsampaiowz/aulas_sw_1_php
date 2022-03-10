59ab34283abdac3e6ae9a7033b482893b8a4c34a
Gabriel Oliveira Sampaio <gabrielsampaio1216@gmail.com>
2022-03-10 09:09:57 -0300

    Add files via upload

---
+++ aula_10_03_22/exemplo_01.php
@@ -0,0 +1,19 @@
+<?php
+
+    // CSS
+    echo "<style> *{background-color: purple;}</style>";
+    echo "<h1 style='text-align: center; color: orange; font-family: monospace; margin-top: 20%;' >";
+
+    $num1 = 5;
+    $num2 = 10;
+
+    $soma = $num1 + $num2;
+
+    echo "A soma de ". $num1 ." + ". $num2 ." é igual a ". $soma ."<br>";
+    
+    echo "A soma de $num1 +  $num2 é igual a $soma <br>";
+
+    echo 'A soma de $num1 +  $num2 é igual a $soma <br>';
+
+
+?>
