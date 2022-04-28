2788c65978f1fc9875879439ce071dab0406f7b5
Gabriel Oliveira Sampaio <gabrielsampaio1216@gmail.com>
2022-04-28 08:08:24 -0300

    Add files via upload

---
+++ revisao/exemplo.php
@@ -0,0 +1,14 @@
+<?php
+    $num = 10;
+
+    if($num > 10){
+        echo "Número é maior que 10";
+    }
+    elseif($num < 10){
+        echo "Número é menor que 10";
+    }
+    else{
+        echo "Número é igual a 10";
+    }
+
+?>
---
+++ revisao/exemplo_02.php
@@ -0,0 +1,19 @@
+<?php
+    $estado = "sp";
+    $estado = strtoupper($estado);
+
+    switch ($estado) {
+        case 'SP':
+            echo "Você é paulista kkkkkkkkkkkkkkkkkk muito cria";
+            break;
+        case 'RJ';
+            echo "Carioca safado kekw";
+            break;
+        case 'MG';
+            echo "Uai tu é Mineiro sô";
+            break;
+        default:
+            echo "Coe daonde tu veio irmão";
+            break;
+    }
+?>
