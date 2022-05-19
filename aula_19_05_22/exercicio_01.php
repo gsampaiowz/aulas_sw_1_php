b9608712141f3107a80a00f9c213c99b763e124d
Gabriel Oliveira Sampaio <gabrielsampaio1216@gmail.com>
2022-05-19 09:10:57 -0300

    Add files via upload

--- aula_19_05_22/exercicio_01.php
+++ aula_19_05_22/exercicio_01.php
@@ -1,5 +1,8 @@
 <?php
 $mes = ["Janeiro","Fevereiro","Março","Abril","Maio","Junho","Julho","Agosto","Setembro","Outubro","Novembro","Dezembro"];
-
-
+$numeroMes = 1;
+if($numeroMes<1 || $numeroMes>12){
+    echo "Você não digitou um número inválido"
+}
+echo $mes[$numeroMes - 1]
 ?>
