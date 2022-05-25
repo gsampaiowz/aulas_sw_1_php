cec7c60f35376271efedf41f6c03d0d8ff74394a
Gabriel Oliveira Sampaio <gabrielsampaio1216@gmail.com>
2022-05-25 20:21:41 -0300

    Delete exercicio_01.php

--- aula_19_05_22/exercicio_01.php
+++
@@ -1,8 +0,0 @@
-<?php
-$mes = ["Janeiro","Fevereiro","Março","Abril","Maio","Junho","Julho","Agosto","Setembro","Outubro","Novembro","Dezembro"];
-$numeroMes = 1;
-if($numeroMes<1 || $numeroMes>12){
-    echo "Você não digitou um número inválido"
-}
-echo $mes[$numeroMes - 1]
-?>
